0db38228fe919d33372b700d454777f02478b553
姚超臣 <yaochaochen@ctv.org.cn>
2019-06-25 17:21:00 +0800

    Flink Of WordCount

---
+++ Flink-train-java/src/main/java/com/ycc/flink/java/case1/WordCountExample.java
@@ -0,0 +1,32 @@
+package com.ycc.flink.java.case1;
+
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.java.DataSet;
+import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.util.Collector;
+
+public class WordCountExample {
+
+    public static void main(String[] args) throws Exception{
+        final ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
+
+        DataSet<String> text = env.fromElements("Who's there?",
+                "I think I hear them. Stand, ho! Who's there?");
+        DataSet<Tuple2<String, Integer>> wordCounts = text.flatMap(new LineSplitter())
+                .groupBy(0).sum(1);
+        wordCounts.print();
+    }
+
+    public static class LineSplitter implements FlatMapFunction<String, Tuple2<String, Integer>> {
+
+        @Override
+        public void flatMap(String s, Collector<Tuple2<String, Integer>> collector) throws Exception {
+                for (String word : s.split(" ")) {
+                    collector.collect(new Tuple2<String, Integer>(word, 1));
+                }
+        }
+    }
+}
+
+
